f1b72e5f2f9f16172bc70fa6c99c05d26c37e2b7
veankatakrishnan <veankatakrishnan@gmail.com>
2026-01-13 15:22:36 +0530

    Fix : fixed the deploying issue with BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Quiz from './components/Quiz';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
